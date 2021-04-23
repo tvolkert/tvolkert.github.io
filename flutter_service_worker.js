3c27e27cfecad1a340c2728f01bfb69eec21b295
Todd Volkert <tvolkert@google.com>
2021-04-22 23:48:02 -0700

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "38a08ec307855d884164f407c4545d77",
-"index.html": "e24789ab1f5499511e38e1934bda0ac9",
-"/": "e24789ab1f5499511e38e1934bda0ac9",
-"main.dart.js": "e9052604a9ea22fc288840c5372e7d35",
+"index.html": "ba58043839ab0821e16ddb73080c3cf5",
+"/": "ba58043839ab0821e16ddb73080c3cf5",
+"main.dart.js": "260efbc2944370a69715176b2d3e72cb",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
